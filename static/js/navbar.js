99a3f53bb89f40393da4b5ff5f9a1d3844e1fbab
Gaganpreet Singh <9592951585g@gmail.com>
2020-11-29 15:59:33 +0530

    added navbar

---
+++ static/js/navbar.js
@@ -0,0 +1,40 @@
+var navitems = document.getElementsByClassName("navitems");
+var special = document.getElementById("special");
+var mobile = document.getElementById("mobile_view");
+mobile.style.display = "none";
+if(window.innerWidth > 550){
+		for(var i = 0; i < navitems.length; i++){
+			navitems[i].style.display = "inline";
+		}
+		special.style.display = "none";
+	}
+	else{
+		for(var i = 0; i < navitems.length; i++){
+			navitems[i].style.display = "none";
+		}
+		special.style.display = "inline";
+	}
+window.addEventListener("resize", ()=>{
+	if(window.innerWidth > 550){
+		for(var i = 0; i < navitems.length; i++){
+			navitems[i].style.display = "inline";
+		}
+		special.style.display = "none";
+		mobile.style.display = "none";
+	}
+	else{
+		for(var i = 0; i < navitems.length; i++){
+			navitems[i].style.display = "none";
+		}
+		special.style.display = "inline";
+	}
+});
+special.addEventListener("click", () =>{
+	if(mobile.style.display == "none"){
+		mobile.style.display = "block";
+	}
+	else{
+		document.getElementById("mobile_view").style.display = "none";
+	}
+});
+console.log(window.innerWidth);
