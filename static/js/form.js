7f0d6e5b2f1a152c13bdcba75cec6ed33ac46aa4
Gaganpreet Singh <9592951585g@gmail.com>
2020-12-02 13:15:15 +0530

    changed form.js and settings.py

--- static/js/form.js
+++ static/js/form.js
@@ -53,7 +53,6 @@ function add_interests(){
 		});
 		document.getElementById("interests").appendChild(wrapper);
 		interest_list.push(document.getElementById("interest-list").value);
-		console.log(interest_list);
 		document.getElementById("interest-list").value = "";
 	}
 	
@@ -61,14 +60,14 @@ function add_interests(){
 
 function submit_func(){
 	var data = {};
-	var inputs = document.getElementByClassName("post_inputs");
+	var inputs = document.getElementsByClassName("post_inputs");
 	for(var i = 0; i < inputs.length; i++){
 		data["" + inputs[i].name] = inputs[i].value;
 	}
 	data["interest_list"] = interest_list;
 	console.log(data);
  	var request = new XMLHttpRequest();
- 	request.open("POST", "/info/", data);
+ 	request.open("POST", "/info/");
  	request.setRequestHeader("Content-Type", "applicatoin/json");
  	//request.send(JSON.stringify(data));  configure the csrf token
 }
