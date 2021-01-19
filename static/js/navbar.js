2fa2bb2a2222806977490bb0ce5d95c75dec9e95
Gaganpreet Singh <9592951585g@gmail.com>
2021-01-19 14:41:28 +0530

    some more changes

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,7 +2,7 @@ var navitems = document.getElementsByClassName("navitems");
 var special = document.getElementById("special");
 var mobile = document.getElementById("mobile_view");
 mobile.style.display = "none";
-if(window.innerWidth > 800 && screen.width > 800){
+if(window.innerWidth > 900 && screen.width > 900){
 		for(var i = 0; i < navitems.length; i++){
 			navitems[i].style.display = "inline";
 		}
@@ -15,7 +15,7 @@ if(window.innerWidth > 800 && screen.width > 800){
 		special.style.display = "inline";
 	}
 window.addEventListener("resize", ()=>{
-	if(window.innerWidth > 800 && screen.width > 800){
+	if(window.innerWidth > 900 && screen.width > 900){
 		for(var i = 0; i < navitems.length; i++){
 			navitems[i].style.display = "inline";
 		}
